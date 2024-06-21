77aa6efa60c005b4cccf728ef6f8043ac18519d7
animesh-2002 <93852453+animesh-2002@users.noreply.github.com>
2024-06-21 19:47:17 +0530

    Adding Main file

---
+++ ProjectRelatedToStudentDatabaseSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.StudentCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectRelatedToStudentDatabaseSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectRelatedToStudentDatabaseSystemApplication.class, args);
+	}
+
+}
